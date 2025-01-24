9f8168291e00964a1f7062945413d63c330bba38
Riccardo Cecchini <rcecchini.ds@gmail.com>
2025-01-24 17:20:32 +0100

    evaluation transform

--- eval.py
+++ eval.py
@@ -35,7 +35,7 @@ class AgePredictor:
             self.transform = transforms.Compose([
                 transforms.Resize(600),
                 transforms.CenterCrop(600),
-                transforms.RandomHorizontalFlip(),
+                transforms.RandomHorizontalFlip(), # it has sense in evaluation?
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
